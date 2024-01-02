2ddbe98ac9276d1fb1dff9c683810098cffba677
manal2002 <112483159+manal2002@users.noreply.github.com>
2024-01-02 20:07:03 +0500

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -2,19 +2,20 @@ import mlflow.sklearn
 import pandas as pd
 from preprocess import preprocess_data
 
+# Assuming the live data is in a CSV file (replace 'path_to_live_data.csv' with the actual path)
+live_data_path = 'path_to_live_data.csv'
+live_data = pd.read_csv(live_data_path)
+
+# Preprocess the live data using the same preprocessing steps as during training
+live_data_processed, _ = preprocess_data(live_data_path)
+
 # Load the registered model from the MLflow Model Registry
 model_name = 'PredictiveMaintenanceModel'
 model_version = 'Production'  # Use the appropriate version
 model = mlflow.sklearn.load_model(f"models:/{model_name}/{model_version}")
 
-# Load live data (replace 'path_to_live_data.csv' with the actual path)
-live_data = pd.read_csv('path_to_live_data.csv')
-
-# Preprocess the live data if needed (use the same preprocessing used during training)
-#X_processed, y = preprocess_data(file_path)
-
 # Make predictions on the live data
-live_predictions = model.predict(live_data)
+live_predictions = model.predict(live_data_processed)
 
 # You can print or further process the predictions as needed
 print("Live Predictions:", live_predictions)
