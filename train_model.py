6ac59947a193b1a0e7edfa460f6a50eb5f0be60e
Noorain Tahir <noorain.tahirr@gmail.com>
2024-01-03 19:54:11 +0500

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -19,13 +19,19 @@ X_processed, y = preprocess_data(file_path)
 # Split the data into training and validation sets
 X_train, X_val, y_train, y_val = train_test_split(X_processed, y, test_size=0.2, random_state=42)
 
+# Save the preprocessed data
+#pd.DataFrame(X_train).to_csv('preprocessed_X_train.csv', index=False)
+#pd.DataFrame(X_val).to_csv('preprocessed_X_val.csv', index=False)
+#pd.DataFrame(y_train).to_csv('preprocessed_y_train.csv', index=False)
+#pd.DataFrame(y_val).to_csv('preprocessed_y_val.csv', index=False)
+
 # Create and train the model
 model = RandomForestRegressor()
 
 # Define hyperparameters for tuning
 param_grid = {
-    'n_estimators': [50, 100, 150],
-    'max_depth': [None, 10, 20, 30]
+  'n_estimators': [50, 100, 150],
+  'max_depth': [None, 10, 20, 30]
 }
 
 # Use GridSearchCV for hyperparameter tuning
@@ -37,29 +43,26 @@ best_model = grid_search.best_estimator_
 
 # Log model parameters and metrics with MLflow
 with mlflow.start_run() as run:
-    # Log preprocessing parameters
-    mlflow.log_params({'data_preprocessing': 'ColumnTransformer with OneHotEncoder'})
+  # Log preprocessing parameters
+  mlflow.log_params({'data_preprocessing': 'ColumnTransformer with OneHotEncoder'})
 
-    # Log best model parameters
-    mlflow.log_params({'n_estimators': best_model.n_estimators, 'max_depth': best_model.max_depth})
+  # Log best model parameters
+  mlflow.log_params({'n_estimators': best_model.n_estimators, 'max_depth': best_model.max_depth})
 
-    # Log training metrics for the best model
-    y_pred_train = best_model.predict(X_train)
-    mse_train = mean_squared_error(y_train, y_pred_train)
-    mlflow.log_metrics({'mse_train': mse_train})
+  # Log training metrics for the best model
+  y_pred_train = best_model.predict(X_train)
+  mse_train = mean_squared_error(y_train, y_pred_train)
+  mlflow.log_metrics({'mse_train': mse_train})
 
-    # Save the best model
-    mlflow.sklearn.log_model(best_model, "best_model")
-
-    # Obtain the run ID
-    run_id = mlflow.active_run().info.run_id
+  # Save the best model
+  mlflow.sklearn.log_model(best_model, "best_model")
 
 # Register the best model in the MLflow Model Registry
 model_name = 'PredictiveMaintenanceModel'
 model_stage = 'Production'
 
 # Get the model URI
-model_uri = f"runs:/{run_id}/best_model"
+model_uri = f"best_model"
 
 # Register the best model in the Model Registry
 mlflow.register_model(model_uri, model_name)
